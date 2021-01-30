f2274dfc947ee7a930819cd835e7c791bc803dcb
16_ <omicron.mc.de@gmail.com>
2021-01-30 16:32:52 +0100

    Change stuff

--- src/main/java/me/sixteen_/insane/mixin/KeyboardMixin.java
+++ src/main/java/me/sixteen_/insane/mixin/KeyboardMixin.java
@@ -1,5 +1,6 @@
 package me.sixteen_.insane.mixin;
 
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,10 +19,12 @@ public class KeyboardMixin {
 
 	@Inject(method = "onKey", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/InputUtil;fromKeyCode(II)Lnet/minecraft/client/util/InputUtil$Key;", shift = At.Shift.AFTER))
 	private void onKey(long window, int key, int scancode, int i, int j, final CallbackInfo callback) {
-		final ModuleManager mm = Insane.getInsane().getModuleManager();
-		for (Module m : mm.getModules()) {
-			if (m.getKeybind().getCode() == key) {
-				m.toggle();
+		if (i == GLFW.GLFW_PRESS) {
+			final ModuleManager mm = Insane.getInsane().getModuleManager();
+			for (Module m : mm.getModules()) {
+				if (m.getKeybind().getCode() == key) {
+					m.toggle();
+				}
 			}
 		}
 	}
