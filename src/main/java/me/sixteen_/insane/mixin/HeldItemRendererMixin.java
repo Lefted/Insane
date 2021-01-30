b5093164833e41d9fef9137221355156a6fd8b55
16_ <omicron.mc.de@gmail.com>
2021-01-30 16:15:16 +0100

    Add CallbackInfo

--- src/main/java/me/sixteen_/insane/mixin/HeldItemRendererMixin.java
+++ src/main/java/me/sixteen_/insane/mixin/HeldItemRendererMixin.java
@@ -3,6 +3,7 @@ package me.sixteen_.insane.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import me.sixteen_.insane.Insane;
 import me.sixteen_.insane.module.modules.render.Inspect;
@@ -20,8 +21,7 @@ import net.minecraft.util.Hand;
 public class HeldItemRendererMixin {
 
 	@Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
-	private void renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress,
-			MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+	private void renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, final CallbackInfo info) {
 		final Inspect inspect = (Inspect) Insane.getInsane().getModuleManager().getModule(Inspect.class);
 		if (inspect.isEnabled()) {
 			inspect.disable(swingProgress, equipProgress);
